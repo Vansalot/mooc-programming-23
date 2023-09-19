484db0e5a519102d4716994602e1e85a6713c74b
Vansalot <inniscor@hotmail.com>
2023-09-19 09:23:21 +0200

    added 'times_ten' assignment

--- part05-14_times_ten/src/times_ten.py
+++ part05-14_times_ten/src/times_ten.py
@@ -1,1 +1,10 @@
-# Write your solution here
+def times_ten(start_index: int, end_index: int):
+    dict = {}
+    for number in range(start_index, end_index + 1):
+        dict[number] = number * 10
+    return dict
+
+
+if __name__=='__main__':
+    d = times_ten(11, 16)
+    print(d)
