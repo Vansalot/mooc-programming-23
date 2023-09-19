ff660fb58fbae8de5100a013e8ce86397eb8aef9
Vansalot <inniscor@hotmail.com>
2023-09-19 09:45:22 +0200

    added 'factorials' assignment

--- part05-15_factorials/src/factorials.py
+++ part05-15_factorials/src/factorials.py
@@ -1,1 +1,12 @@
-# Write your solution here
+def factorials(n: int):
+    factorial_dict = {}
+    factorial_dict[1] = 1
+    for number in range(2, n + 1):
+        factorial_dict[number] = factorial_dict[number - 1] * number
+    return factorial_dict
+
+if __name__ == '__main__':
+    k = factorials(5)
+    print(k[1])
+    print(k[3])
+    print(k[5])
