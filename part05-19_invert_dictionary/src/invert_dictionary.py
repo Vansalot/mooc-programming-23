2c11cf81b3ba5b45e507515191f03a24d2abd6ee
Vansalot <inniscor@hotmail.com>
2023-09-22 21:32:23 +0200

    added invert dictionary assignment

--- part05-19_invert_dictionary/src/invert_dictionary.py
+++ part05-19_invert_dictionary/src/invert_dictionary.py
@@ -1,1 +1,15 @@
-# Write your solution here
+def invert(dictionary: dict):
+    inverted_dict = {}
+
+    for key, item in dictionary.items():
+        inverted_dict[item] = key
+    dictionary.clear()
+    for key, item in inverted_dict.items():
+        dictionary[key] = item
+
+
+if __name__ == "__main__":
+    s = {1: "first", 2: "second", 3: "third", 4: "fourth"}
+    print(s)
+    invert(s)
+    print(s)
