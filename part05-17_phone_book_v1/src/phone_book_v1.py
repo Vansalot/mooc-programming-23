c5e746f2aad25973c13ebb6a6f9cf5f9ef6cb565
Vansalot <inniscor@hotmail.com>
2023-09-22 20:51:40 +0200

    addded phone book 1 assignment

--- part05-17_phone_book_v1/src/phone_book_v1.py
+++ part05-17_phone_book_v1/src/phone_book_v1.py
@@ -1,1 +1,27 @@
-# Write your solution here
+def add_number(phonebook):
+    name = input('name: ')
+    phonenumber = input('number: ')
+    phonebook[name] = phonenumber
+    print('ok!')
+
+def search_number(phonebook):
+    name = input('name: ')
+    if name in phonebook:
+        print(phonebook[name])
+    else:
+        print('no number')
+
+def main():
+    phonebook = {}
+    while True:
+        command = input('Command (1 search, 2 add, 3 quit): ')
+        if command == '3':
+            break
+        elif command == '2':
+            add_number(phonebook)
+        elif command == '1':
+            search_number(phonebook)
+    print('quitting...')            
+
+
+main()
