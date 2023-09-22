d33b857be42c14dcc008239ad367f88fd7171e9b
Vansalot <inniscor@hotmail.com>
2023-09-22 20:25:41 +0200

    added histogram assignment

--- part05-16_histogram/src/histogram.py
+++ part05-16_histogram/src/histogram.py
@@ -1,1 +1,14 @@
-# Write your solution here
+def histogram(word: str):
+    histogram_dict = {}
+    for letter in word:
+        if letter not in histogram_dict:
+            histogram_dict[letter] = 1
+        elif letter in histogram_dict:
+            histogram_dict[letter] += 1
+    
+    for letter, times in histogram_dict.items():
+        print(letter, '*' * times)
+
+if __name__ == '__main__':
+    histogram("abba")
+    histogram("statistically")
