9ea56997e50487ced79baa9f7e3733d13f40bd71
Vansalot <inniscor@hotmail.com>
2023-09-23 11:37:56 +0200

    added oldest person assignment

--- part05-24_oldest_person/src/oldest_person.py
+++ part05-24_oldest_person/src/oldest_person.py
@@ -1,1 +1,21 @@
-# Write your solution here
+def oldest_person(people: list):
+    oldest = people[0]
+    for person in people:
+        if person[1] < oldest[1]:
+            oldest = person
+    
+    return oldest[0]
+
+
+
+if __name__=='__main__':
+    people_list = [("Arthur", 1977), ("Emily", 2014)]
+    print(oldest_person(people_list))
+
+    p1 = ("Adam", 1977)
+    p2 = ("Ellen", 1985)
+    p3 = ("Mary", 1953)
+    p4 = ("Ernest", 1997)
+    people = [p1, p2, p3, p4]
+
+    print(oldest_person(people))
