61b044f095f75863cf1eaa982872ecca4b1e7dc1
Vansalot <inniscor@hotmail.com>
2023-09-23 11:16:50 +0200

    added create touple assignment

--- part05-23_create_tuple/src/create_tuple.py
+++ part05-23_create_tuple/src/create_tuple.py
@@ -1,1 +1,9 @@
-# Write your solution here
+def create_tuple(x: int, y: int, z: int):
+    largest = max([x, y, z])
+    smallest = min([x, y, z])
+    sumarium = sum([x, y, z])
+
+    return (smallest, largest, sumarium)
+
+if __name__=='__main__':
+    print(create_tuple(5, 3, -1))
